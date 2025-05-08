a24b7b13246aae5371efb6b6edf55748d6b978fb
Cablaida-Meryrose <cablaida.meryrose.dll@gmail.com>
2025-05-08 22:52:36 +0800

    some more fixes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-Router-Application/',
   plugins: [react(), tailwindcss()],
 })
