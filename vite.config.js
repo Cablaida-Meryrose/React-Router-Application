1f88b1b8272314e0bcdf990b7fe0f2113d48d99f
Cablaida-Meryrose <cablaida.meryrose.dll@gmail.com>
2025-05-08 22:56:49 +0800

    sana last nato

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Router-Application/',
   plugins: [react(), tailwindcss()],
 })
